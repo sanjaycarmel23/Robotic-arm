e7be3da528ab2f0e73666e77950d24ffce39b749
Sanjay Carmel <110335915+sanjaycarmel23@users.noreply.github.com>
2024-08-07 11:23:05 +0530

    Add files via upload

---
+++ Opencv.py
@@ -0,0 +1,46 @@
+from cvzone.HandTrackingModule import HandDetector
+import cv2
+from cvzone.SerialModule import SerialObject
+
+# Initialize the webcam to capture video
+# The '2' indicates the third camera connected to your computer; '0' would usually refer to the built-in camera
+cap = cv2.VideoCapture(0)
+
+# Initialize the HandDetector class with the given parameters
+detector = HandDetector(staticMode=False, maxHands=1, modelComplexity=1, detectionCon=0.7, minTrackCon=0.5)
+
+arduino = SerialObject(portNo="COM8", baudRate=9600, digits=1, max_retries=5) # CHANGE PORT NO
+
+# Continuously get frames from the webcam
+while True:
+    # Capture each frame from the webcam
+    # 'success' will be True if the frame is successfully captured, 'img' will contain the frame
+    success, img = cap.read()
+
+    # Find hands in the current frame
+    # The 'draw' parameter draws landmarks and hand outlines on the image if set to True
+    # The 'flipType' parameter flips the image, making it easier for some detections
+    hands, img = detector.findHands(img, draw=True, flipType=True)
+
+    # Check if any hands are detected
+    if hands:
+
+        # Information for the first hand detected
+        hand1 = hands[0]  # Get the first hand detected
+        lmList1 = hand1["lmList"]  # List of 21 landmarks for the first hand
+        bbox1 = hand1["bbox"]  # Bounding box around the first hand (x,y,w,h coordinates)
+        center1 = hand1['center']  # Center coordinates of the first hand
+        handType1 = hand1["type"]  # Type of the first hand ("Left" or "Right")
+
+        # Count the number of fingers up for the first hand
+        fingers1 = detector.fingersUp(hand1)
+        print(fingers1)
+        arduino.sendData(fingers1)
+
+        print(" ")  # New line for better readability of the printed output
+
+    # Display the image in a window
+    cv2.imshow("Image", img)
+
+    # Keep the window open and update it for each frame; wait for 1 millisecond between frames
+    cv2.waitKey(1)
